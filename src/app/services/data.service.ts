a4e762c4a4742b35a3c3a8fca8247100178eb2f1
dissectorz <toshikverma@yahoo.com>
2018-02-03 14:28:33 +0530

    2

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -9,7 +9,7 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/observable/throw';
 
 @Injectable()
-abstract  class DataService {
+export abstract  class DataService {
   constructor(private url: string, private http: Http) { }
 
   getAll(obj) {
