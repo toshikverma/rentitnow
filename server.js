7422ebf74c39bd5bfefbc11189b284c4ec69fbc1
dissectorz <toshikverma@yahoo.com>
2018-02-04 14:45:40 +0530

    yes

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
  const app = express();
- 
  app.use(express.static(__dirname + '/dist'));
+ app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
  app.listen(process.env.PORT || 8080);
